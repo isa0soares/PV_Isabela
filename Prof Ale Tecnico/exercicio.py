023b2f00f96fcb648035cb2a30483e1f56c9fd84
isa0soares <148557925+isa0soares@users.noreply.github.com>
2023-10-20 11:06:46 -0300

    Add files via upload

---
+++ Prof Ale Tecnico/exercicio.py
@@ -0,0 +1,15 @@
+#exercicio 1
+#print("O primeiro programa a gente nunca esquece!")
+
+#exercicio 2
+#print("Isabela Pereira Soares")
+#print("Bauru")
+#print("isabelapereirasoares2@gmail.com")
+
+#exercicio 3
+
+##print("XXXXX")
+#print("X   X")
+#print("X   X")
+#print("X   X")
+#print("XXXXX")
